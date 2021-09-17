39193ca8b2f4bb32c1b30f135ee72ac57b92df0f
julesukco <email9@customsite.com>
2021-09-16 20:17:15 -0600

    handle currency, and refactor tests to use __eq__ method

--- test/test_money.py
+++ test/test_money.py
@@ -1,19 +1,36 @@
 import unittest
 
 
-class Dollar:
-    def __init__(self, amount):
+class Money:
+    def __init__(self, amount, currency):
         self.amount = amount
+        self.currency = currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
 
 
 class TestMoney(unittest.TestCase):
     def test_should_multiply(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+        fiver = Money(5, "USD")
+        tenner = Money(10, "USD")
+        self.assertEqual(tenner, fiver.times(2))
+
+    def test_should_multiply_in_euros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = Money(20, "EUR")
+        self.assertEqual(twentyEuros, tenEuros.times(2))
+
+    def test_should_divide(self):
+        twenty_dollars = Money(20, "USD")
+        ten_dollars = Money(10, "USD")
+        self.assertEqual(ten_dollars, twenty_dollars.divide(2))
 
 
 if __name__ == '__main__':
